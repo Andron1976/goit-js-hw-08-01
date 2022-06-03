6ef32b360ddecf452e1abc5283ac30f032727b19
Andron1976 <and.ron@ukr.net>
2022-06-03 15:39:31 +0300

    did task 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let stopFrame = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(insertElement, 1000));
+
+function insertElement({ seconds }) {
+  console.log('hello start');
+
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(stopFrame);
