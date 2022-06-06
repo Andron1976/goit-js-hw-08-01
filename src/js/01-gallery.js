b3021d40a84aaef06bb3762733f56f4d80fd6ad5
Andron1976 <and.ron@ukr.net>
2022-06-06 17:09:25 +0300

    fixed 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,21 +4,24 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import '../css/01-gallery.css';
 import '../css/common.css';
 
-const galleryEl = document.querySelector('.gallery');
+// make p.p. 1,2
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
 
 function newItem() {
-    return galleryItems.map(
-        (pics) =>
-          `<a class="gallery__item" href="${pics.original}">
-          <img class="gallery__image" src="${pics.preview}" alt="${pics.description}" />
-          </a>`
-      ).join("");
-  }
+  return galleryItems.map(
+      (pics) =>
+        `<a class="gallery__item" href="${pics.original}">
+        <img class="gallery__image" src="${pics.preview}" alt="${pics.description}" />
+        </a>`
+    ).join("");
+}
+galleryEl.insertAdjacentHTML("afterbegin", newItem(galleryItems));
 
-galleryEl.insertAdjacentHTML('afterbegin', makeGalleryCard(galleryItems));
-
+// make 3,4
 let lightbox = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
   });
-  console.log(galleryItems);
